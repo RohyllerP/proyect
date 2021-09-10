8865dda4b61617567978875064304ab65a7ee08d
RohyllerP <87720304+RohyllerP@users.noreply.github.com>
2021-09-10 10:16:29 -0430

    ss

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Box, Heading } from "@chakra-ui/react"
 export default function Home() {
   return (
     <Box>
-      <Heading as="h1" size="2xl">Hola</Heading>
+      <Heading as="h1" size="2xl">Holaaa</Heading>
     </Box>
   )
 }
