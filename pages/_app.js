f2f2904f7cac9e08e2d7c9c56705a9e00e41f1d8
RohyllerP <87720304+RohyllerP@users.noreply.github.com>
2021-09-10 10:20:47 -0430

    oo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
-
+import { ChakraProvider } from "@chakra-ui/react"
 function MyApp({ Component, pageProps }) {
+  <ChakraProvider>
   return <Component {...pageProps} />
+   </ChakraProvider>
 }
 
 export default MyApp
